860407fbf87021a159686d7c5c6d3d2de7b1a008
Carlos Eduardo <anjoazrael10@gmail.com>
2025-09-30 20:23:02 -0300

    feat(logging): configure file and console logging in main.py

--- src/control_custo/main.py
+++ src/control_custo/main.py
@@ -1,10 +1,25 @@
-from fastapi import FastAPI
-from .adapters.web import api
 import logging
 
-app = FastAPI(root_path='/api')
+from fastapi import FastAPI
+from .adapters.web import api
 
-logger = logging.getLogger("uvicorn.error")  # logger do servidor
+
+file_log = 'store/logs/app.log'
+
+# Configuração básica de logging - DEBUG, INFO, WARNING, ERROR, CRITICAL
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler(file_log, mode="a", encoding="utf-8"),
+        logging.StreamHandler()  # imprime no console (stdout)
+    ]
+)
+
+logger = logging.getLogger(__name__)
 access_logger = logging.getLogger("uvicorn.access")  # acessos HTTP
+
+app = FastAPI(root_path='/api')
 
 app.include_router(api.router)
